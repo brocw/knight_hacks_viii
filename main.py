d4f8e6ff30ea1ef7a97da97191f2e870e36dbcad
Broc <thebattlefrog@gmail.com>
2025-10-25 01:40:25 -0400

    Stats for NumPy arrays

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 
+def stat(mat, name):
+    print(f"Stats for {name}")
+    print(f"Count: {len(mat)}")
+    print(f"Mean: {np.mean(mat)}")
+    print(f"Median: {np.median(mat)}")
+    print(f"Std Dev: {np.std(mat)}")
+    print(f"Min: {np.min(mat)}")
+    print(f"Max: {np.max(mat)}")
+    print(f"{mat}\n")
+
+
 def main():
     print("Hello World!")
     distance_matrix = np.load("distance_matrix.npy")
@@ -10,6 +21,12 @@ def main():
     photo_indexes = np.load("photo_indexes.npy")
 
     print("Loaded data!")
+
+    stat(distance_matrix, "Distance Matrix")
+    stat(predecessors, "Predecessors")
+    stat(points_lat_long, "Points (Lat/Long)")
+    stat(asset_indexes, "Asset Indexes")
+    stat(photo_indexes, "Photo Indexes")
 
     # TODO: VISUALIZE (visual.py?)
 
